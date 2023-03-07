1f95f74d127e66f1422bbada7958cb6b53a9809e
jack1ew <jackiew@email.sc.edu>
2023-03-07 14:55:22 -0500

    Update

--- inc/client.h
+++ inc/client.h
@@ -59,7 +59,7 @@ class DomainSocketClient : public UnixDomainSocket {
     int seSize = se.size();
     int t = 0;
     while (true) {
-      std::cout << se << std::endl;
+      std::cout << op << std::endl;
       if(ep < seSize) {
         bufferWriter(sp, ep , se, write_buffer);
         ep += kWrite_buffer_size;
