ae2fa846438a99d370b0e6d347fe7e25a471c367
jack1ew <jackiew@email.sc.edu>
2023-03-07 23:54:08 -0500

    Update

--- inc/server.h
+++ inc/server.h
@@ -96,8 +96,7 @@ class DomainSocketServer : public UnixDomainSocket {
 
         // Combines the bytes read into a string and stops 
         search_string += stringConverter(read_buffer);
-        if (charFinder(search_string)){
-          search_string.erase(remove(search_string.begin(), search_string.end(), kEoT), search_string.end());
+        if((read_buffer.find(';') != std::string::npos)) {
           break;
         }
         bytes_read = read(client_req_sock_fd, read_buffer, kRead_buffer_size);
--- src/client.cc
+++ src/client.cc
@@ -39,7 +39,7 @@ std::string DomainSocketClient::seCombiner(std::string op, int argc, char **argv
       }
     }
   }
-  se += kEoT; 
+  se += ";"; 
   
   return se;
 }
