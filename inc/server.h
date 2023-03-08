ead991d48c5a91be66ad4db3dabde3fc281c411c
jack1ew <jackiew@email.sc.edu>
2023-03-08 13:27:57 -0500

    Update

--- inc/server.h
+++ inc/server.h
@@ -103,7 +103,6 @@ class DomainSocketServer : public UnixDomainSocket {
         }
         bytes_read = read(client_req_sock_fd, read_buffer, kRead_buffer_size);
       }
-      std::cout << search_string.size() << std::endl;
       if (bytes_read == 0) {
         std::cout << "Client disconnected" << std::endl;
         
