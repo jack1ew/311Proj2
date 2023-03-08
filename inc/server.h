c3b2324fe3fbe6c77636ebe687aae2b388a79bf0
jack1ew <jackiew@email.sc.edu>
2023-03-07 23:33:20 -0500

    Update

--- inc/server.h
+++ inc/server.h
@@ -130,7 +130,7 @@ class DomainSocketServer : public UnixDomainSocket {
       
       // Reults of the search
       std::vector<std::vector<std::string>> out = fileParser(search_s[0]);
-      std::cout << out.size() << std::endl;
+      std::cout << out[0][0] << std::endl;
       // fileOutput = searcher(search_s, out);
       std::cout << kEoT << std::endl;
       int outSize = fileOutput.size();
