e72d1886f51167a65db1278f6355aa188c770a9c
jack1ew <jackiew@email.sc.edu>
2023-03-09 14:22:04 -0500

    Update

--- src/client.cc
+++ src/client.cc
@@ -9,6 +9,7 @@ std::string DomainSocketClient::operationFinder(int argc, char **argv) {
   }
   for (int i = 4; i < argc - 1; i+=2) {
     if (argv[i] == argv[4]) {
+      std::cout << argv[i] << std::endl;
       continue;
     } else {
       b = false;
